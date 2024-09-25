a780953482abba97d44fbdfa343d2adc0f436a76
Vladymir <vlady_noel789@gmail.com>
2024-09-25 12:15:04 +0800

    setup 2

---
+++ cloneflut/lib/utils/constants/image.strings.dart
@@ -0,0 +1,5 @@
+class TImages {
+  //App Logo
+  static const String vadeLogo = "assets/logo/logo.png";
+  static const String vadeLogoName = "assets/logo/logoname.png";
+}
---
+++ cloneflut/lib/utils/theme/theme.dart
@@ -0,0 +1,29 @@
+import 'package:cloneflut/utils/theme/custom_themes/appbar_theme.dart';
+import 'package:cloneflut/utils/theme/custom_themes/elevated_button_theme.dart';
+import 'package:cloneflut/utils/theme/custom_themes/text.theme.dart';
+import 'package:flutter/material.dart';
+
+class TAppTheme {
+  TAppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: TTextTheme.lightTextTheme,
+    elevatedButtonTheme: TElevatedButtonTheme.lightElevatedButtonTheme,
+    appBarTheme: TAppBarTheme.lightAppBarTheme,
+  );
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: TTextTheme.darkTextTheme,
+    elevatedButtonTheme: TElevatedButtonTheme.darkElevatedButtonTheme,
+    appBarTheme: TAppBarTheme.darkAppBarTheme,
+  );
+}
