6edbfdd57517baf1565d168d677b92b374636650
Vladymir Roman <vlady.noel789@gmail.com>
2024-09-25 12:49:53 +0800

    working splash

--- cloneflut/lib/main.dart
+++ cloneflut/lib/main.dart
@@ -1,19 +1,8 @@
-import 'package:cloneflut/utils/theme/theme.dart';
-import 'package:flutter/material.dart';
 
+import 'package:flutter/material.dart';
+import 'app.dart';
 void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-    );
-  }
-}
